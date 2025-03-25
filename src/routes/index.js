e4ea637e16036245a736d17ff4cfa738e1b5d095
Tanvir <tanvir.info07@gmail.com>
2025-03-25 16:07:15 +0600

    bug fix and prisma install

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,22 @@
 import express from "express";
 import userRoutes from "./userRoutes.js";
 import instructorRoutes from "./instructorRoutes.js";
-import { notFound } from "../middleware/errorHandler.
 import categoryRouter from "./categoryRouter.js";
 import subCategoryRouter from "./subCategoryRoute.js";
 
 import courseRoutes from "./courseRoutes.js";
+import { notFound } from "../middleware/errorHandler.js";
 
 const router = express.Router();
 
 // API routes
 router.use("/users", userRoutes); //api/v1/users
 
-router.use("/category", categoryRouter);  
+router.use("/category", categoryRouter);
 router.use("/sub_category", subCategoryRouter);
 
 router.use("/instructors", instructorRoutes); //api/v1/instructors
 router.use("/courses", courseRoutes); //api/v1/instructors
-
 
 // Handle 404 for API routes
 router.use(notFound);
