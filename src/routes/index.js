6ecf1848fac3c3460e3d9f662c8dcf5f1a7c7958
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2025-03-25 15:52:19 +0600

    bugfixd

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
-import userRoutes from "./userRoutes.js";
 import instructorRoutes from "./instructorRoutes.js";
-import { notFound } from "../middleware/errorHandler.
+import userRoutes from "./userRoutes.js";
+
 import categoryRouter from "./categoryRouter.js";
 import subCategoryRouter from "./subCategoryRoute.js";
 
+import { notFound } from "./../middleware/errorHandler.js";
 import courseRoutes from "./courseRoutes.js";
 
 const router = express.Router();
@@ -12,12 +13,11 @@ const router = express.Router();
 // API routes
 router.use("/users", userRoutes); //api/v1/users
 
-router.use("/category", categoryRouter);  
+router.use("/category", categoryRouter);
 router.use("/sub_category", subCategoryRouter);
 
 router.use("/instructors", instructorRoutes); //api/v1/instructors
 router.use("/courses", courseRoutes); //api/v1/instructors
-
 
 // Handle 404 for API routes
 router.use(notFound);
